b41e0376e2852e9375bc3fbb5443f70fee1ec356
Charles Perry <charles@Charless-MacBook-Pro.local>
2020-06-12 12:38:25 -0700

    Tried adding the collision map, to no avail, but the logic is there

--- game.js
+++ game.js
@@ -1,8 +1,199 @@
 let game = document.getElementById("game");
 let ctx = game.getContext("2d");
-game.width = 1000;
-game.height = 700;
+game.width = 960;
+game.height = 720;
 
+//Map layout and collision logic
+world = function(){
+  var map = {
+    cols: 20,
+    rows: 15,
+    tile_size: 48,
+    tiles: [
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ] 
+
+  }
+  var collisionMap = {
+    cols: 20,
+    rows: 15,
+    tile_size: 48,
+    tiles: [
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 9, 1, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    ] 
+  }
+}
+world.prototype = {
+  constuctor : world,
+  collideWithObject:function(object) {//check locations of the corners of each object
+    
+    var top, right, bottom, left;
+    
+    //top left corner
+    
+    top = Math.floor(object.getTop() / this.tile_size);
+    left = Math.floor(object.getLeft() / this.tile_size);
+    value = this.collision_map[top * this.columns + left];
+    this.collider.collide(value, object, left * this.tile_size, top * this.tile_size, this.tile_size);
+    
+    //top right corner
+    
+    top    = Math.floor(object.getTop()    / this.tile_size);
+    right  = Math.floor(object.getRight()  / this.tile_size);
+    value  = this.collision_map[top * this.columns + right];
+    this.collider.collide(value, object, right * this.tile_size, top * this.tile_size, this.tile_size);
+
+    //bottom left corner
+
+    bottom = Math.floor(object.getBottom() / this.tile_size);
+    left   = Math.floor(object.getLeft()   / this.tile_size);
+    value  = this.collision_map[bottom * this.columns + left];
+    this.collider.collide(value, object, left * this.tile_size, bottom * this.tile_size, this.tile_size);
+
+    //bottom right corner
+
+    bottom = Math.floor(object.getBottom() / this.tile_size);
+    right  = Math.floor(object.getRight()  / this.tile_size);
+    value  = this.collision_map[bottom * this.columns + right];
+    this.collider.collide(value, object, right * this.tile_size, bottom * this.tile_size, this.tile_size);
+
+  }
+
+}
+//----------Time for some collsion functions
+//collison type based on collison_map values includes all possible collision border combinations
+collider = function() {
+  this.collide = function(value, object, tile_x, tile_y, tile_size) {
+    switch(value) {
+      case  1: this.collidePlatformTop      (object, tile_y            ); break;
+      case  2: this.collidePlatformRight    (object, tile_x + tile_size); break;
+      case  3: if (this.collidePlatformTop  (object, tile_y            )) return;// If there's a collision, we don't need to check for anything else.
+               this.collidePlatformRight    (object, tile_x + tile_size); break;
+      case  4: this.collidePlatformBottom   (object, tile_y + tile_size); break;
+      case  5: if (this.collidePlatformTop  (object, tile_y            )) return;
+               this.collidePlatformBottom   (object, tile_y + tile_size); break;
+      case  6: if (this.collidePlatformRight(object, tile_x + tile_size)) return;
+               this.collidePlatformBottom   (object, tile_y + tile_size); break;
+      case  7: if (this.collidePlatformTop  (object, tile_y            )) return;
+               if (this.collidePlatformRight(object, tile_x + tile_size)) return;
+               this.collidePlatformBottom   (object, tile_y + tile_size); break;
+      case  8: this.collidePlatformLeft     (object, tile_x            ); break;
+      case  9: if (this.collidePlatformTop  (object, tile_y            )) return;
+               this.collidePlatformLeft     (object, tile_x            ); break;
+      case 10: if (this.collidePlatformLeft (object, tile_x            )) return;
+               this.collidePlatformRight    (object, tile_x + tile_size); break;
+      case 11: if (this.collidePlatformTop  (object, tile_y            )) return;
+               if (this.collidePlatformLeft (object, tile_x            )) return;
+               this.collidePlatformRight    (object, tile_x + tile_size); break;
+      case 12: if (this.collidePlatformLeft (object, tile_x            )) return;
+               this.collidePlatformBottom   (object, tile_y + tile_size); break;
+      case 13: if (this.collidePlatformTop  (object, tile_y            )) return;
+               if (this.collidePlatformLeft (object, tile_x            )) return;
+               this.collidePlatformBottom   (object, tile_y + tile_size); break;
+      case 14: if (this.collidePlatformLeft (object, tile_x            )) return;
+               if (this.collidePlatformRight(object, tile_x            )) return;
+               this.collidePlatformBottom   (object, tile_y + tile_size); break;
+      case 15: if (this.collidePlatformTop  (object, tile_y            )) return;
+               if (this.collidePlatformLeft (object, tile_x            )) return;
+               if (this.collidePlatformRight(object, tile_x + tile_size)) return;
+               this.collidePlatformBottom   (object, tile_y + tile_size); break;
+    }
+  }
+};
+collider.prototype = {
+  constructor : collider,
+  collidePlatformBottom:function(object, tile_bottom) {
+    if(object.getTop() < tile_bottom && object.getOldTop() >= tile_bottom) {
+      object.setTop(tile_bottom);
+      object.y_velocity = 0;
+      return true;
+    }return false;
+  },
+  collidePlatformLeft:function(object, tile_left){
+    if(object.getRight() < tile_bottom && object.getOldRight() >= tile_left){
+      object.setRight(tile_left);
+      object.x_velocity = 0;
+      return true;
+    }return false;
+  },
+  collidePlatformTop:function(object, tile_top){
+    if(object.getBottom() < tile_top && object.getOldBottom() >= tile_top){
+      object.setBottom(tile_top);
+      object.y_velocity = 0;
+      return true;
+    }return false;
+  },
+  collidePlatformRight:function(object, tile_right){
+    if(object.getLeft() < tile_right && object.getOldLeft() >= tile_right){
+      object.setLeft(tile_right);
+      object.x_velocity = 0;
+      return true;
+    }return false;
+  }
+}
+
+
+Object = function(x, y, color, width, height) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.color = color.fillStyle("grey");
+  this.x_old = x;
+  this.y_old = y;
+};
+//time to handle the different sides of each object
+Object.prototype = {
+  constructor : Object,
+
+  getBottom:   function()  { return this.y     + this.height; },
+  getLeft:     function()  { return this.x;                   },
+  getRight:    function()  { return this.x     + this.width;  },
+  getTop:      function()  { return this.y;                   },
+  getOldBottom:function()  { return this.y_old + this.height; },
+  getOldLeft:  function()  { return this.x_old;               },
+  getOldRight: function()  { return this.x_old + this.width;  },
+  getOldTop:   function()  { return this.y_old                },
+  setBottom:   function(y) { this.y     = y    - this.height; },
+  setLeft:     function(x) { this.x     = x;                  },
+  setRight:    function(x) { this.x     = x    - this.width;  },
+  setTop:      function(y) { this.y     = y;                  },
+  setOldBottom:function(y) { this.y_old = y    - this.height; },
+  setOldLeft:  function(x) { this.x_old = x;                  },
+  setOldRight: function(x) { this.x_old = x    - this.width;  },
+  setOldTop:   function(y) { this.y_old = y;                  }
+
+};
+
+// Handles creation of new characters
 function Entity(x, y, color, width, height) {
     this.x = x;
     this.x_velocity = 0;
@@ -17,8 +208,8 @@ function Entity(x, y, color, width, height) {
         ctx.fillStyle = this.color
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
-}
-let mainChar = new Entity(576, 0, "red", 48, 48);
+};
+let mainChar = new Entity(432, 0, "red", 48, 48);
 mainChar.x_velocity = 0;
 mainChar.y_velocity = 0;
 controller = {
@@ -67,23 +258,23 @@ gameLoop = function() {
     mainChar.y_velocity *= 0.9;// 
   
     // if mainChar is falling below floor line
-    if (mainChar.y > 700 - 24 - 48) {
+    if (mainChar.y > 720 - 24 - 48) {
   
       mainChar.jumping = false;
-      mainChar.y = 700 - 24 - 48;
+      mainChar.y = 720 - 24 - 48;
       mainChar.y_velocity = 0;
   
     }
     // if mainChar is going off the left of the screen
     if (mainChar.x < -48) {  
-        mainChar.x = 1000;
-    } else if (mainChar.x > 1000) {// if mainChar goes past right boundary
+        mainChar.x = 960;
+    } else if (mainChar.x > 960) {// if mainChar goes past right boundary
   
       mainChar.x = -48;
   
     }
     mainChar.render();
-}
+};
 let gameTick = setInterval(gameLoop, 16)
 window.addEventListener("keydown", controller.keyListener);
 window.addEventListener("keyup", controller.keyListener);
